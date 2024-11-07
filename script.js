24d52d09aaafc6c641c7274a81e336d700a5006f
froza <thenewforza@gmail.com>
2024-11-07 13:36:04 +0300

    feat: скрипт для header

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 (function() {
-    const observer = new PerformanceObserver((list) => {
-        list.getEntriesByType('navigation').forEach((entry) => {
-            const loadTime = entry.domContentLoadedEventEnd - entry.startTime;
-            document.getElementById('load-time').textContent = 'Page load time is ' + (loadTime / 1000).toFixed(2) + ' seconds.';
+    if (window.PerformanceObserver) {
+        const observer = new PerformanceObserver((list) => {
+            list.getEntriesByType('navigation').forEach((entry) => {
+                const loadTime = entry.domContentLoadedEventEnd - entry.startTime;
+                document.getElementById('load-time').textContent = 'Page load time is ' + (loadTime / 1000).toFixed(2) + ' seconds.';
+            });
         });
-    });
 
-    observer.observe({ type: 'navigation', buffered: true });
+        observer.observe({ type: 'navigation', buffered: true });
+    }
+
+    function setActiveMenuItem() {
+        const navItems = document.querySelectorAll('.navbar__menu a');
+        const currentPage = window.location.pathname.split('/').pop();
+
+        navItems.forEach(item => {
+            const linkPage = item.getAttribute('href').split('/').pop();
+            if (linkPage === currentPage) {
+                item.classList.add('active');
+            }
+        });
+    }
+
+    window.addEventListener('load', setActiveMenuItem);
 })();
